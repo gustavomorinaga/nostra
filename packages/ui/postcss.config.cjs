71b9c0e3b6d59162b8b4cc6d50e4b8efb51ec8bd
Gustavo Morinaga <me@gustavomorinaga.dev>
2023-11-02 04:59:45 -0300

    chore: update ui styles and dependencies

--- packages/ui/postcss.config.cjs
+++ packages/ui/postcss.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 module.exports = {
 	plugins: [
+		require('postcss-import'),
 		require('tailwindcss/nesting'),
 		require('tailwindcss'),
 		require('autoprefixer'),
